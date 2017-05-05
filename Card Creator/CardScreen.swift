5bd4fd7fa36a85dd9395f0bf22cc10373ca4d672
MikaelWeiss <campingmikael@icloud.com>
2017-05-05 08:10:49 -0600

    added the basic UI

--- Card Creator/CardScreen.swift
+++ Card Creator/CardScreen.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CardScreen.swift
 //  Card Creator
 //
 //  Created by Mikael Weiss on 5/4/17.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CardScreen: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
